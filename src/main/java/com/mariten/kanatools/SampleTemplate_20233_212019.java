508d738ab9a373e8b945ecbb1e1eb7f75c96f088
saurabh <saurabh.patil@acellere.com>
2023-03-21 20:19:39 +0530

    Added file to verify countt issue

---
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20233_212019.java
@@ -0,0 +1,11 @@
+package com.mariten.kanatools;
+
+/**
+  * Sample java  file  to test add/carry forward,reopen,fixed issues.
+  */
+public class SampleTemplate_20233_212019
+{
+  public static void addTest_SampleTemplate_20233_212019(char eval_char){
+          System.out.println(" Added code issue ");
+} 
+ }
