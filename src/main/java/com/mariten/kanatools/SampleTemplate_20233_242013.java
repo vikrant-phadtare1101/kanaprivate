f44d44171d9fdc7c8c50c47dea2ead7bbe5fbb01
saurabh <saurabh.patil@acellere.com>
2023-03-24 20:21:22 +0530

    Fixed earlier introduced issue

--- src/main/java/com/mariten/kanatools/SampleTemplate_20233_242013.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20233_242013.java
@@ -1,15 +1,7 @@
-package com.mariten.kanatools;
-
-/**
-  * Sample java  file  to test add/carry forward,reopen,fixed issues.
-  */
-public class SampleTemplate_20233_242013
-{
   public static void addTest_SampleTemplate_20233_242013(char eval_char){
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
-          System.out.println(" Added code issue ");
 } 
- }
+ } }
