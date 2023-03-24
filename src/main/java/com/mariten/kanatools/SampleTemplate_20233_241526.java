b8a9c04326034d5cb923037db1a65d314f4adcff
saurabh <saurabh.patil@acellere.com>
2023-03-24 15:46:27 +0530

    Fixed earlier introduced issue

--- src/main/java/com/mariten/kanatools/SampleTemplate_20233_241526.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20233_241526.java
@@ -5,7 +5,4 @@ package com.mariten.kanatools;
   */
 public class SampleTemplate_20233_241526
 {
-  public static void addTest_SampleTemplate_20233_241526(char eval_char){
-          System.out.println(" Added code issue ");
-} 
  }
