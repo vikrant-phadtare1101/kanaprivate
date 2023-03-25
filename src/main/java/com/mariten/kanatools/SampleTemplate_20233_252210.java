fee21dfa89c22e7a687f3734d8f8e29dc71bf0f4
root <root@ip-10-1-20-212.eu-central-1.compute.internal>
2023-03-25 22:11:32 +0000

    "Fixed earlier introduced issue"

--- src/main/java/com/mariten/kanatools/SampleTemplate_20233_252210.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20233_252210.java
@@ -10,6 +10,5 @@ public class SampleTemplate_20233_252210
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
-          System.out.println(" Added code issue ");
 } 
  }
