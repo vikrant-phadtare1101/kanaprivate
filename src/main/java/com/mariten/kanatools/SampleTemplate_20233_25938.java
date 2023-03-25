3f8b987e03d91b1a6eacd43802a4f258ae1a9888
root <root@ip-10-1-20-212.eu-central-1.compute.internal>
2023-03-25 09:38:46 +0000

    "Added file to verify count issue"

---
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20233_25938.java
@@ -0,0 +1,15 @@
+package com.mariten.kanatools;
+
+/**
+  * Sample java  file  to test add/carry forward,reopen,fixed issues.
+  */
+public class SampleTemplate_20233_25938
+{
+  public static void addTest_SampleTemplate_20233_25938(char eval_char){
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+} 
+ }
