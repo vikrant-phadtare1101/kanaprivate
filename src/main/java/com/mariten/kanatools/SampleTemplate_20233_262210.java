eb58ef9870942fff8c8acf09c81355601184ace6
root <root@ip-10-1-20-212.eu-central-1.compute.internal>
2023-03-26 22:12:06 +0000

    "Fixed earlier introduced issue"

--- src/main/java/com/mariten/kanatools/SampleTemplate_20233_262210.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20233_262210.java
@@ -10,6 +10,5 @@ public class SampleTemplate_20233_262210
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
-          System.out.println(" Added code issue ");
 } 
  }
