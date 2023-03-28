dc94146da788145c1a07eaeedc669aef4118a91b
saurabh <saurabh.patil@acellere.com>
2023-03-28 18:02:31 +0530

    Fixed earlier introduced issue

--- src/main/java/com/mariten/kanatools/SampleTemplate_20233_281758.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20233_281758.java
@@ -10,5 +10,6 @@ public class SampleTemplate_20233_281758
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
 } 
  }
