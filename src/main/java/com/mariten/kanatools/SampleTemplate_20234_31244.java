88384533c160142b4e2c6d62dd6735cf20172dca
saurabh <saurabh.patil@acellere.com>
2023-04-03 12:44:44 +0530

    Added file to verify count issue

---
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20234_31244.java
@@ -0,0 +1,15 @@
+package com.mariten.kanatools;
+
+/**
+  * Sample java  file  to test add/carry forward,reopen,fixed issues.
+  */
+public class SampleTemplate_20234_31244
+{
+  public static void addTest_SampleTemplate_20234_31244(char eval_char){
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+} 
+ }
