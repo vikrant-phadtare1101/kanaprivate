6d1c72cae8ea68dfb2089ce64ebfdb39bf89d457
Pooja Jadhav <jadhavpoojasopan@gmail.com>
2023-04-06 15:13:28 +0530

    Fixed earlier introduced issue

--- src/main/java/com/mariten/kanatools/SampleTemplate_20234_61511.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20234_61511.java
@@ -10,5 +10,6 @@ public class SampleTemplate_20234_61511
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
 } 
  }
