f504e482062c6e07894efbdd703ce6e6e66a0805
Pooja Jadhav <jadhavpoojasopan@gmail.com>
2023-04-06 15:04:12 +0530

    Added file to verify count issue

---
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20234_6154.java
@@ -0,0 +1,15 @@
+package com.mariten.kanatools;
+
+/**
+  * Sample java  file  to test add/carry forward,reopen,fixed issues.
+  */
+public class SampleTemplate_20234_6154
+{
+  public static void addTest_SampleTemplate_20234_6154(char eval_char){
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+} 
+ }
