890b9b2e93215641de140a2fa8ac2a7da34447ce
saurabh.patil <saurabh.pa@browserstack.com>
2023-05-11 17:29:02 +0530

    "Fixed earlier introduced issue"

--- src/main/java/com/mariten/kanatools/SampleTemplate_20235_111725.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20235_111725.java
@@ -10,5 +10,6 @@ public class SampleTemplate_20235_111725
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
 } 
  }
