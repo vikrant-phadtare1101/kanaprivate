a41747330e8f3d2545fb52b56be1627e6f3d530c
Jenkins <jenkins@ip-10-1-25-75.eu-central-1.compute.internal>
2024-09-30 06:52:33 +0000

    "re-introduced earlier fixed issue"

--- src/main/java/com/mariten/kanatools/SampleTemplate_20249_30648.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20249_30648.java
@@ -10,5 +10,6 @@ public class SampleTemplate_20249_30648
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
 } 
  }
