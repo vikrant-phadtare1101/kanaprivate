dcfb8cc5c003c7c0bd104a02625436509359a244
Jenkins <jenkins@ip-10-1-25-75.eu-central-1.compute.internal>
2024-12-10 18:40:16 +0000

    "Added file to verify count issue"

---
+++ src/main/java/com/mariten/kanatools/SampleTemplate_202412_101840.java
@@ -0,0 +1,15 @@
+package com.mariten.kanatools;
+
+/**
+  * Sample java  file  to test add/carry forward,reopen,fixed issues.
+  */
+public class SampleTemplate_202412_101840
+{
+  public static void addTest_SampleTemplate_202412_101840(char eval_char){
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+} 
+ }
