b9447b56dab55a4b081508a31343163f209867a9
vikrant1717 <vikrantphadtare9@gmail.com>
2025-04-05 19:49:47 +0530

    "Fixed earlier introduced issue"

--- src/main/java/com/mariten/kanatools/SampleTemplate_20254_51949.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20254_51949.java
@@ -10,6 +10,5 @@ public class SampleTemplate_20254_51949
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
-          System.out.println(" Added code issue ");
 } 
  }
