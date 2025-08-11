034bebc2ff7405c8f0ee43ae0a7100f030cb3e97
Ubuntu <ubuntu@ip-10-1-25-75.eu-central-1.compute.internal>
2025-08-11 12:30:47 +0000

    "Fixed earlier introduced issue"

--- src/main/java/com/mariten/kanatools/SampleTemplate_20258_111227.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20258_111227.java
@@ -10,6 +10,5 @@ public class SampleTemplate_20258_111227
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
-          System.out.println(" Added code issue ");
 } 
  }
