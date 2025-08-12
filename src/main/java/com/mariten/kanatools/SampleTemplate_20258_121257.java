c51ba5465d4bff5b7c1df3ee6c105339ede90df5
vikrant1717 <vikrantphadtare9@gmail.com>
2025-08-12 12:57:27 +0530

    "Added file to verify count issue"

---
+++ src/main/java/com/mariten/kanatools/SampleTemplate_20258_121257.java
@@ -0,0 +1,15 @@
+package com.mariten.kanatools;
+
+/**
+  * Sample java  file  to test add/carry forward,reopen,fixed issues.
+  */
+public class SampleTemplate_20258_121257
+{
+  public static void addTest_SampleTemplate_20258_121257(char eval_char){
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+          System.out.println(" Added code issue ");
+} 
+ }
